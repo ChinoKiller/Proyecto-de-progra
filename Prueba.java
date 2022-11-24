a56ef3ca3929f3be43f80e0d810016124745d37b
ErickHH18 <ERICK.HERNANDEZHERNANDEZ@ucr.ac.cr>
2022-11-23 20:10:16 -0600

    Avance 1

--- Prueba.java
+++
@@ -1,8 +0,0 @@
-public class Prueba{
-        public static void main(String[] args){
-            System.out.println("Hola");
-            System.out.println("Mundo");
-            System.out.println("Mundano");
-            System.out.println("Chino gay jaja xd");
-        }
-}
